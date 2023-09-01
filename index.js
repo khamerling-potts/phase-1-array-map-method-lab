a5eb84ab1ca5f06c24f6261480c390171bda9e9b
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2023-09-01 12:22:12 -0400

    array map lab done

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(title => {
+    const words = title.split(' ');
+    const titleCasedWords = words.map(word => {
+      return word[0].toUpperCase() + word.slice(1);
+    });
+    return titleCasedWords.join(' ');
+  });
 }
+
+console.log(titleCased());
